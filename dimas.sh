7c3133f13e61e4478d952f2db908a35823b25ef1
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-11 00:57:34 +0800

    Create dimas.sh

---
+++ dimas.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+unzip XLArig-v5.2.2-linux-x86_64.zip
+./xlarig -o scala.herominers.com:10131 -u Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic -p agiz3 -a panthera -k
